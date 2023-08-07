1bcf619cc6e700c3d354a3e9deebcb34b2a45d65
besence <141612879+besence@users.noreply.github.com>
2023-08-08 00:04:22 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function openrule() {
     window.location.href = "serverip.html";
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeElements = document.querySelectorAll(".fade-in-element");
+  
+    fadeElements.forEach(function(element) {
+      element.style.opacity = 1; // 要素を可視にする
+    });
+  });
+  
--- serverip.js
+++ serverip.js
@@ -1,3 +1,12 @@
 function homeOpen() {
     window.location.href = "index.html";
 };
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeElements = document.querySelectorAll(".fade-in-element");
+  
+    fadeElements.forEach(function(element) {
+      element.style.opacity = 1; // 要素を可視にする
+    });
+  });
